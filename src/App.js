d90450396205619f3ce861244fd3d6afd507a1c7
Kevin <kevin.castro.chavez0@gmail.com>
2022-09-13 17:26:32 -0600

    learning how useState works

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return 'Hi';
+  const [count, setCount] = useState(0);
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div>
+      <h2>The count is {count}</h2>
+
+      <button onClick={decrement}>-</button>
+      <button onClick={increment}>+</button>
+    </div>
+  );
 }
 
 export default App;
